883d51e157795dab5d27a88b7ca0ef59d19b9ed4
kldd0 <steparesh@gmail.com>
2022-10-29 17:38:35 +0300

    dockerized frontend

--- app/backend/src/main.py
+++ app/backend/src/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
-def home():
-    return "Test"
+async def home():
+    return {"status": "ok"}
